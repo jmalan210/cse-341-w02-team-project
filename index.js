00a63ce098f303f5ba5338d4a3ea1d216603f8e2
jmalan210 <>
2026-05-13 17:22:22 -0600

    fixing .env issues

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 app
   .use(cors())
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
-  .use('/', require('./routes'));
+  .use('/', require('./routes'))
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const db = require('./models');
 db.mongoose
